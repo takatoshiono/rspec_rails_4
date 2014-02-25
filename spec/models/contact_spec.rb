5c1be58c4fa5b8a6ba3efa26098bf6b04ac91797
Takatoshi Ono <takatoshi.ono@paperboy.co.jp>
2014-02-26 06:54:34 +0900

    Add some tests for a Contact model

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,10 +1,26 @@
 require 'spec_helper'
 
 describe Contact do
-  it "is valid with a firstname, lastname and email"
-  it "is invalid without a firstname"
+  it "is valid with a firstname, lastname and email" do
+    contact = Contact.new(
+      firstname: 'Aaron',
+      lastname: 'Summer',
+      email: 'tester@example.com')
+    expect(contact).to be_valid
+  end
+
+  it "is invalid without a firstname" do
+    expect(Contact.new(firstname: nil)).to have(1).errors_on(:firstname)
+  end
+
   it "is invalid without a lastname"
   it "is invalid without an email address"
-  it "is invalid with a duplicate email address"
+
+  it "is invalid with a duplicate email address" do
+    Contact.create(firstname: 'Joe', lastname: 'Tester', email: 'tester@example.com')
+    contact = Contact.new(firstname: 'Jane', lastname: 'Tester', email: 'tester@example.com')
+    expect(contact).to have(1).errors_on(:email)
+  end
+
   it "returns a contact's full name as a string"
 end
